7fbb13364a4bd611982d971b7701571337cbcc1b
J-J-T-M <jonatham.junior.18@gmail.com>
2023-01-18 16:10:41 -0400

    create controller

---
+++ app/Http/Controllers/FinalityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Finality;
+use Illuminate\Http\Request;
+
+class FinalityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Finality  $finality
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Finality $finality)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Finality  $finality
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Finality $finality)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Finality  $finality
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Finality $finality)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Finality  $finality
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Finality $finality)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PropertyProximityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PropertyProximity;
+use Illuminate\Http\Request;
+
+class PropertyProximityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PropertyProximity  $propertyProximity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PropertyProximity $propertyProximity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PropertyProximity  $propertyProximity
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PropertyProximity $propertyProximity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PropertyProximity  $propertyProximity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PropertyProximity $propertyProximity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PropertyProximity  $propertyProximity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PropertyProximity $propertyProximity)
+    {
+        //
+    }
+}
