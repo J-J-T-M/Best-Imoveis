7aeae71bdd0a4953962d146f02d9652519aa0b54
J-J-T-M <jonatham.junior.18@gmail.com>
2023-01-18 16:11:00 -0400

    create model

---
+++ app/Models/PropertyProximity.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PropertyProximity extends Model
+{
+    use HasFactory;
+}
