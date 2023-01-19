276b3fe439b9f609c357697344b696f2e014dcf3
J-J-T-M <jonatham.junior.18@gmail.com>
2023-01-19 15:38:26 -0400

    add resultados por pagina

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Define o número de resultado por página exibido na index
+     * como resultado de uma pesquisa usando o filtro
+     *
+     * @var integer
+     */
+    static protected $results_per_page = 10;
 }
