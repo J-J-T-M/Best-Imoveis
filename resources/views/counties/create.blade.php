ebd68db70e0fb13205fe4efce1efc82d02c327f8
J-J-T-M <jonatham.junior.18@gmail.com>
2023-01-20 15:10:05 -0400

    reformulando a view

--- resources/views/counties/create.blade.php
+++ resources/views/counties/create.blade.php
@@ -4,15 +4,15 @@
         <form action="{{ route('counties.store') }}" method="POST">
             <div class="card">
                 <div class="card-title">
-                    Novo município
+                    Nova cidade
                 </div>
                 <div class="card-body">
                     @csrf
                     @include('counties._form')
                 </div>
                 <div class="right-align">
-                    <button type="submit" class="btn waves-effect waves-light" id="btn-ok">OK</button>
                     <a href="{{ route('counties.index') }}" class="btn-flat waves-effect">Voltar</a>
+                    <button type="submit" class="btn waves-effect waves-light" id="btn-ok">OK</button>
                 </div>
             </div>
         </form>
